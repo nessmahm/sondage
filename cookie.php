0833f6cb2972aa56d1bd72fbceea59713a0abd3b
nessmahm <99190580+nessmahm@users.noreply.github.com>
2022-11-30 15:30:35 +0100

    Add files via upload

--- cookie.php
+++ cookie.php
@@ -1,18 +1,41 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+    <title>Submition</title>
+    <style>
+        .sondage {
+        margin:auto  ;
+        background-color: rgb(230, 169, 211);
+        max-width: 600px;
+        margin-top: 50px;
+        justify-content: center;
+        align-items: center;
+        border-radius: 10px;
+        padding: 50px;
+    }
+       </style>
+</head>
+<body>
+   
 <?php   
 
-if(!isset($_COOKIE["vote"]))    {
-setcookie("vote",$_POST["vote"],time()+60*2);
-header("location: index.html"); 
-}
- else {
-      ?>
-       <script>  
-          alert("Vous avez deja voté a <?=  $_COOKIE["vote"] ?>!!!!!")  ; </script>
-    
-
-<?php }
-
-
+   if(!isset($_COOKIE["vote"]))    {
+      setcookie("vote",$_POST["vote"],time()+60*2);
+      $msg="successfully submitted 👍";
+   }
+   else 
+      $msg="Vous avez deja voté a " .  $_COOKIE["vote"] . " !!" ;
 ?>
+      
 
+
+
+    <div class="sondage">
+        <h1><?= $msg ?></h1>
+    </div>
+</body>
+</html>
